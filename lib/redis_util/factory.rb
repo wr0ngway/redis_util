c06606a4feee7269d23b761f38005df7f2b09462
james-lawrence <james.lawrence@backupify.com>
2014-02-06 13:02:14 -0500

    add retries to reconnect

--- lib/redis_util/factory.rb
+++ lib/redis_util/factory.rb
@@ -2,26 +2,26 @@ require 'redis'
 require 'redis-namespace'
 
 module RedisUtil
-  
+
   # A factory class for creating redis connections, useful for
   # reconnecting them after a fork
   #
   class Factory
     include GemLogger::LoggerSupport
     extend MonitorMixin
-  
+
     class << self
-      
+
       # Set the path to the yml config file which defines all the redis connections.
       # If running in rails, it will default to "#{Rails.root}/config/redis.yml"
       attr_accessor :config_file
-      
+
       # Set the environment we are running in (development/test/etc).
       # If running in rails, it will default to Rails.env
       attr_accessor :env
-      
+
       attr_accessor :clients, :configuration
-      
+
       # Creates/retrieves a single redis client for the given named configuration
       #
       # @param [Symbol] name The name of the redis configuration (config/redis.yml) to use
@@ -36,7 +36,7 @@ module RedisUtil
         end
         clients[name].first
       end
-    
+
       # Always create and return a new redis client for the given named configuration
       #
       # @param [Symbol] name The name of the redis configuration (config/redis.yml) to use
@@ -49,7 +49,7 @@ module RedisUtil
         end
         clients[name].last
       end
-    
+
       # Disconnect all known redis clients
       #
       # @param [Symbol] key (optional, defaults to all) The name of the redis configuration (config/redis.yml) to disconnect
@@ -58,7 +58,7 @@ module RedisUtil
         synchronize do
           clients.clone.each do |name, client|
             next if key && name != key
-    
+
             connections = clients.delete(name) || []
             connections.each do |connection|
               begin
@@ -73,7 +73,7 @@ module RedisUtil
         logger.debug "RedisUtil::Factory.disconnect complete"
         nil
       end
-    
+
       # Reconnect all known redis clients
       #
       # @param [Symbol] key (optional, defaults to all) The name of the redis configuration (config/redis.yml) to reconnect
@@ -82,20 +82,29 @@ module RedisUtil
         synchronize do
           clients.each do |name, connections|
             next if key && name != key
-            
+
             connections.each do |connection|
               logger.debug "Reconnecting Redis client: #{connection}"
-              connection.client.reconnect
+              retry_count = 0
+              begin
+                connection.client.reconnect
+              rescue => e
+                if retry_count < 3
+                  sleep(Random.rand(5))
+                  retry_count += 1
+                  retry
+                end
+              end
             end
           end
         end
-        logger.debug "RedisUtil::Factory.reconnect complete"    
+        logger.debug "RedisUtil::Factory.reconnect complete"
       end
 
       def configuration
         synchronize do
           @configuration ||= begin
-                                
+
             if config_file.nil? || env.nil?
               if defined?(Rails)
                 self.config_file ||= "#{Rails.root}/config/redis.yml"
@@ -104,7 +113,7 @@ module RedisUtil
                 raise "You need to initialize with an env and config file"
               end
             end
-            
+
             self.clients = {}
             require 'erb'
             config = YAML::load(ERB.new(IO.read(config_file)).result)
@@ -112,15 +121,15 @@ module RedisUtil
           end
         end
       end
-      
+
       private
-      
+
       def lookup_config(name)
         conf = configuration[name]
         raise "No redis configuration for #{env} environment in redis.yml for #{name}" unless conf
         conf
       end
-      
+
       def new_redis_client(conf)
         redis = ::Redis.new(conf)
         if conf[:namespace]
@@ -128,7 +137,7 @@ module RedisUtil
         end
         redis
       end
-          
+
       def symbolize(hash)
         hash.inject({}) do |options, (key, value)|
           value = symbolize(value) if value.kind_of?(Hash)
@@ -138,7 +147,7 @@ module RedisUtil
       end
 
     end
-    
+
   end
 
 end
